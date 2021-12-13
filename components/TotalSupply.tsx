7495c004dfd9eb8318bf903dd5740e875ca5269c
Cole McCracken <colemccracken@coles-mbp-2.lan>
2021-12-13 15:58:25 -0800

    fix build

--- components/TotalSupply.tsx
+++ components/TotalSupply.tsx
@@ -2,10 +2,15 @@ import { useEffect, useState } from "react";
 import { getContract, getProvider } from "./contract";
 export default function TotalSupply() {
   const [totalMinted, setTotalMinted] = useState(0);
-  useEffect(async () => {
-    const contract = await getContract(getProvider(window));
-    const total = await contract.totalSupply();
-    setTotalMinted(total.toNumber());
+
+  useEffect(() => {
+    const fetchSupply = async () => {
+      const contract = await getContract(getProvider(window));
+      const total = await contract.totalSupply();
+      setTotalMinted(total.toNumber());
+    };
+
+    fetchSupply();
   });
 
   return <p>{totalMinted} / 1000 minted </p>;
